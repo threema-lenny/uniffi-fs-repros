ee3d0d652f4bd2136daf5edadc7ed7c843f4a789
Lennart Grahl <lennart.grahl@threema.ch>
2023-11-13 23:31:47 +0100

    Foreign trait issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+use std::sync::Arc;
+
+// namespace functions.
+fn get_buttons() -> Vec<Arc<dyn Button>> {
+    vec![Arc::new(StopButton {}), Arc::new(GoButton {})]
 }
+
+fn press(button: Arc<dyn Button>) -> Arc<dyn Button> {
+    button
+}
+
+pub trait Button: Send + Sync {
+    fn name(&self) -> String;
+}
+
+struct GoButton {}
+
+impl Button for GoButton {
+    fn name(&self) -> String {
+        "go".to_string()
+    }
+}
+
+struct StopButton {}
+
+impl Button for StopButton {
+    fn name(&self) -> String {
+        "stop".to_string()
+    }
+}
+
+uniffi::include_scaffolding!("lib");
